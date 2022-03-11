0d7e96a6f71075d7edc9f7789d3b1f14c1d8187c
UltraViolet33 <ulysse.valdenairepro@gmail.com>
2022-03-11 11:35:52 +0100

    define ROOT path in index?php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,19 @@
 <?php
+   
+session_start();
+// path on MAMP
+// $path = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 
 
-    echo "This is my e shop";
+// define("ASSETS", $path . "assets/");
 
+// include "../app/init.php";
 
-    ?>
+// path on WAMP
+$path = $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+
+$path = str_replace("index.php", "", $path);
+
+define("ROOT", $path);
+
+// $app = new App();
