82907176161a6d903f00d425a3d17aa5906c4895
UltraViolet33 <ulysse.valdenairepro@gmail.com>
2022-03-11 13:56:05 +0100

    display fake books on home page

--- app/views/home.php
+++ app/views/home.php
@@ -1,3 +1,77 @@
 <?php $this->view("inc/header", $data); ?>
-<h1>This is the home page</h1>
+
+<div class="row justify-content-center">
+    <div class="col-6 text-center">
+        <h1 class="">Vite mon Book !</h1>
+        <h2>Site de vente de livres en ligne</h2>
+    </div>
+</div>
+<div class="container">
+    <div class="row my-5 justify-content-center">
+        <h3 class="text-center">Nouveaux Livres</h3>
+        <div class="col-12 col-sm-6 col-lg-4 my-3">
+            <div class="card">
+                <img src="assets/img/book.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Titre du livre</h5>
+                    <p class="card-text">Petit aperçu</p>
+                    <a href="#" class="btn btn-info">Voir plus</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-4 my-3">
+            <div class="card">
+                <img src="assets/img/book.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Titre du livre</h5>
+                    <p class="card-text">Petit aperçu</p>
+                    <a href="#" class="btn btn-info">Voir plus</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-4 my-3">
+            <div class="card">
+                <img src="assets/img/book.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Titre du livre</h5>
+                    <p class="card-text">Petit aperçu</p>
+                    <a href="#" class="btn btn-info">Voir plus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row my-5 justify-content-center">
+        <h3 class="text-center">Livres les plus achetés</h3>
+        <div class="col-12 col-sm-6 col-lg-4 my-3">
+            <div class="card">
+                <img src="assets/img/book.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Titre du livre</h5>
+                    <p class="card-text">Petit aperçu</p>
+                    <a href="#" class="btn btn-info">Voir plus</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-4 my-3">
+            <div class="card">
+                <img src="assets/img/book.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Titre du livre</h5>
+                    <p class="card-text">Petit aperçu</p>
+                    <a href="#" class="btn btn-info">Voir plus</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-4 my-3">
+            <div class="card">
+                <img src="assets/img/book.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Titre du livre</h5>
+                    <p class="card-text">Petit aperçu</p>
+                    <a href="#" class="btn btn-info">Voir plus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php $this->view("inc/footer", $data); ?>
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -9,13 +9,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body style="overflow-x:hidden">
     <div class="container-fluid">
         <div class="row">
             <div class="clo-12">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
+                        <a class="navbar-brand" href="#">Vite mon Book !</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
@@ -25,13 +25,13 @@
                                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Features</a>
+                                    <a class="nav-link" href="#">Categorie</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Pricing</a>
+                                    <a class="nav-link" href="#">Login</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link disabled">Disabled</a>
+                                    <a class="nav-link">Sign Up</a>
                                 </li>
                             </ul>
                         </div>
