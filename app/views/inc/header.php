84306a9111746386e25395dd63c8682a3244effa
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-03-11 21:03:36 +0100

    add profil page

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vite mon Book - <?= $pageTitle ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/main.css">
 </head>
 
 <body style="overflow-x:hidden">
@@ -30,7 +31,7 @@
 
                                 <?php if (isset($data['userData'])) : ?>
                                     <li class="nav-item">
-                                        <a href="signUp" class="nav-link"><?= $data['userData']->pseudoMember ?></a>
+                                        <a href="profil" class="nav-link"><?= $data['userData']->pseudoMember ?></a>
                                     </li>
                                 <?php endif; ?>
                                 <?php if (isset($data['userData'])) : ?>
