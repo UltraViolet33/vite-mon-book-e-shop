09c3903c8d2c1856b36d6dbceaa3531d81288cf3
UltraViolet33 <ulysse.valdenairepro@gmail.com>
2022-03-11 11:48:46 +0100

    add init.php

---
+++ app/init.php
@@ -0,0 +1,7 @@
+<?php
+
+include "../app/core/config.php";
+include "../app/core/functions.php";
+// include "../app/core/controller.php";
+// include "../app/core/database.php";
+include "../app/core/app.php";
