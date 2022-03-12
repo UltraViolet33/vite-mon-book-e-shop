27da283cbba1916a6643d8def398f1432aad02c1
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-03-12 11:09:41 +0100

    ad js ajax to add cat

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,49 @@
-
-
-
 /**
  * disPlayForm
  * add or remove the showFormCat css class to the aff form categories in admin
- * @return 
+ * @return
  */
-function displayForm()
-{
-    const formCat_div = document.querySelector('.formCat');
-    formCat_div.classList.toggle('showFormCat');
+function displayForm() {
+  const formCat_div = document.querySelector(".formCat");
+  formCat_div.classList.toggle("showFormCat");
+}
+/**
+ * sendDataAjax
+ * send the data to the Ajax controller PHP
+ * @param  {object} data={}
+ * @return void
+ */
+function sendDataAjax(data = {}) {
+  const ajax = new XMLHttpRequest();
+  ajax.onload = function () {
+    alert(ajax.responseText);
+  };
+  ajax.open("POST", "<?= ROOT ?>ajax", true);
+  ajax.send(JSON.stringify(data));
+}
+
+/**
+ * collectDataCat
+ * get the input of add cat form
+ * and send the data with sendDataAjax
+ * @return void
+ */
+function collectDataCat() {
+  const categoryAdd_input = document.getElementById("inputAddCat");
+
+  if (
+    categoryAdd_input.value.trim() == "" ||
+    !isNaN(categoryAdd_input.value.trim())
+  ) {
+    alert("Entrez un nom de categorie valide !");
+  } else {
+    const data = categoryAdd_input.value.trim();
+    console.log(data);
+    const objData = {
+      data: data,
+      dataType: "addCategory",
+    };
+    console.table(objData);
+    sendDataAjax(objData);
+  }
 }
