786ae63d227e208a5deee84606bb5541225f7fdb
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-03-20 10:49:40 +0100

    display users

---
+++ app/views/admin/users.php
@@ -0,0 +1,39 @@
+<?php $this->view("inc/header", $data); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center">Users - Admin</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <button class="btn btn-primary"> <a href="<?= ROOT ?>admin/">View Admins</a></button>
+            <button class="btn btn-primary"> <a href="<?= ROOT ?>admin/">View Customers</a></button>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Statut</th>
+                        <th scope="col">Pseudo</th>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Prénom</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Ville</th>
+                        <th scope="col">Code Postal</th>
+                        <th scope="col">Adress</th>
+                    </tr>
+                </thead>
+                <tbody id="tableProducts">
+                    <?php
+                   echo $users;
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php $this->view("inc/footer", $data); ?>
