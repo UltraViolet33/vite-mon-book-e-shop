19a07136e55324f3712553c566f89e012d73acda
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-03-21 21:18:01 +0100

    end for today

--- app/core/config.php
+++ app/core/config.php
@@ -5,6 +5,7 @@ define("WEBSITE_TITLE", "Vite mon Book");
 //database
 define("DB_NAME", "vitemonbookdb");
 define("DB_USER", "root");
+//put your root password
 define("DB_PASS", "root");
 define("DB_TYPE", 'mysql');
 define("DB_HOST", "localhost");
